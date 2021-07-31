6dcf06246ddb48084ad063c034706406e5b46dee
teesloane <tylersloane@gmail.com>
2021-07-30 23:20:57 -0400

    Clean some more

--- resources/public/js/cljs-runtime/shadow.module.renderer.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('renderer');
-
-try { renderer.core.start_BANG_(); } catch (e) { console.error("An error occurred when calling (renderer.core/start!)"); throw(e); }
