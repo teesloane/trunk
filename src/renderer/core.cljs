9880f1f8970ee0790a8ef03cac194908c136959a
teesloane <tylersloane@gmail.com>
2021-08-05 21:20:12 -0400

    Add: stub router; fix event leak.

--- src/renderer/core.cljs
+++ src/renderer/core.cljs
@@ -5,18 +5,49 @@
    [renderer.ipc :as ipc]
    [reagent.dom :as rd]))
 
+(defonce app-db
+  (r/atom {:current-view "article-list"
+           :articles [{:title "Article 1" :content "Pretium varius nam diam urna est arcu"}
+                      {:title "Article 2" :content "Posuere suspendisse phasellus sociosqu sed sollicitudin nam"}
+                      ]}))
 
-(defn article-submit
+(defn navigate!
+  [destination]
+  (swap! app-db assoc :current-view destination))
+
+(defn view-nav
+  []
+  [:nav
+   [:button {:on-click #(navigate! "article-list")} "Articles"]
+   [:button {:on-click #(navigate! "article-create")} "New +"]])
+
+(defn view-article-list
+  []
+  [:div
+   (map (fn [article]
+          [:div
+           [:div (article :title)]
+           [:div (article :content)]]) (@app-db :articles))])
+
+(defn view-article-create
   []
   [:div
    [:textarea]
-   [:button {:on-click #(ipc/send! "<-article-create" "payload")} "Submit"]])
+   [:button {:on-click #(ipc/send! "<-article-create" "payload")} "Submit"]]
+  )
+
+(defn views
+  []
+  (case (@app-db :current-view)
+    "article-list"   [view-article-list]
+    "article-create" [view-article-create]
+    ))
 
 (defn root-component []
   []
   [:main
-   [:div "hi"]
-   [article-submit]])
+   [view-nav]
+   [views]])
 
 (defn ^:dev/after-load start! []
   (ipc/init)
--- src/renderer/ipc.cljs
+++ src/renderer/ipc.cljs
@@ -3,7 +3,7 @@
 (defn send!
   [channel data]
   (let [ipcRenderer (.. (js/require "electron") -ipcRenderer)
-        _         (.send ipcRenderer channel (clj->js data))]))
+        _           (.send ipcRenderer channel (clj->js data))]))
 
 
 (defonce ipcHandlers
@@ -14,14 +14,15 @@
 ;; setup our applications to receive vals.
 
 (defn init
-  "Load ipcRenderer and loop through defined handlers,
-  then "
+  "Load ipcRenderer and loop through defined handlers
+  and attach handlers with handle."
   []
-  ;; TODO: this seems to bind multiple times with hot reload.
   (println "Initing renderer ipc handlers.")
-  (let [ipcRenderer (.. (js/require "electron") -ipcRenderer)]
+  (let [ipcRenderer    (.. (js/require "electron") -ipcRenderer)
+        existingEvents (.eventNames ipcRenderer)]
     (doseq [[key handler] ipcHandlers]
-      (.on ipcRenderer key
-           (fn [event args]
-             (println "[IPC]: " key)
-             (handler event (js->clj args :keywordize-keys true)))))))
+      (when-not (some #{key} existingEvents)
+        (.on ipcRenderer key
+             (fn [event args]
+               (println "[IPC]: " key)
+               (handler event (js->clj args :keywordize-keys true))))))))
