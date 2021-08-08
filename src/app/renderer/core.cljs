6500fd6d3b9e681685dca461023fa703c03b0a44
teesloane <tylersloane@gmail.com>
2021-08-08 09:01:36 -0400

    Add: Stying improvements.

--- src/app/renderer/core.cljs
+++ src/app/renderer/core.cljs
@@ -7,7 +7,7 @@
 (defonce app-db
   (r/atom {:current-view "article-list"
            :articles [{:title "Article 1" :content "Pretium varius nam diam urna est arcu"}
-                      {:title "Article 2" :content "Posuere suspendisse phasellus sociosqu sed sollicitudin nam"}
+                      {:title "Article 2" :content "Posuere suspendisse phasellus sociosqu sed sollicitudin nam Sem purus vestibulum dignissim mattis convallis sociosqu Habitasse ac sollicitudin tempor et commodo nostra"}
                       ]}))
 
 (defn navigate!
@@ -16,29 +16,47 @@
 
 (defn view-nav
   []
-  [:nav.inline-flex
-   [:button.bg-gray-300.hover:bg-gray-400.text-gray-800.font-bold.py-2.px-4.rounded-l {:on-click #(navigate! "article-list")} "Articles"]
-   [:button.bg-gray-300.hover:bg-gray-400.text-gray-800.font-bold.py-2.px-4.rounded-r {:on-click #(navigate! "article-create")} " +"]]
+  [:nav.w-full.bg-gray-200.text-xs
+   [:div.inline-flex.p-2
+    [:button.bg-gray-700.hover:bg-gray-800.text-white.font-bold.py-1.px-2.rounded-l {:on-click #(navigate! "article-list")} "Articles"]
+    [:button.bg-gray-700.hover:bg-gray-800.text-white.font-bold.py-1.px-2.rounded-r {:on-click #(navigate! "article-create")} " +"]]]
 )
+
+(defn container
+  [& children]
+  [:div {:class "mt-8 flex flex-col w-9/12 mx-auto"}
+   children])
+
+(defn page-heading
+  [text]
+  [:h2.text-2xl.mb-2 text])
 
 (defn view-article-list
   []
-  [:div
-   (map (fn [article]
-          [:div
-           [:div (article :title)]
-           [:div (article :content)]]) (@app-db :articles))])
+  (let [stz {:class "table-cell border-b border-gray-100 py-2"}]
+    [container
+     [:div.text-center [page-heading "Your articles"]]
+     [:div.table.w-full.pt-8
+      [:div.table-row
+       [:div.font-bold stz "Article title"]
+       [:div.font-bold stz "Excerpt"]]
+      (map (fn [article]
+             [:div.table-row
+              [:div stz (article :title)]
+              [:div.max-w-xs.truncate stz (article :content)]]) (@app-db :articles))]]))
 
 (defn view-article-create
   []
   (let [article-text (r/atom "foo")
         handle-change #(reset! article-text (-> % .-target .-value))]
     (fn []
-      [:div
-       [:textarea {:name @article-text :on-change handle-change}]
-       [:div "hi there ->" @article-text]
-       [:button {:class "text-xs bg-white hover:bg-gray-100 text-gray-800 py-1 px-2 border border-gray-400 rounded shadow"
-                 :on-click #(ipc/send! "<-article-create" @article-text)} "Submit"]])))
+      [container
+       [:div
+        [:div.text-center [page-heading "Create a new article"]]
+        [:textarea.w-full.p-3.text-gray-700.border.rounded-lg.focus:outline-none.text-sm.my-6
+         {:name @article-text :on-change handle-change :rows 8 :placeholder "Paste article here..."}]
+        [:button {:class "text-xs bg-white hover:bg-gray-100 text-gray-800 py-1 px-2 border border-gray-400 rounded shadow"
+                  :on-click #(ipc/send! "<-article-create" @article-text)} "Submit"]]])))
 
 (defn views
   []
@@ -51,7 +69,8 @@
   []
   [:main
    [view-nav]
-   [views]])
+   [:div.p-2
+    [views]]])
 
 (defn ^:dev/after-load start! []
   (ipc/init)
