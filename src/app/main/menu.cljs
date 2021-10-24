c22a092dc1a06a09d6f44b56358e25d86856c7bd
teesloane <tylersloane@gmail.com>
2021-10-24 18:41:18 -0400

    Fix[windows]: handle for no-nil case in menu template.

--- src/app/main/menu.cljs
+++ src/app/main/menu.cljs
@@ -4,16 +4,16 @@
 
 (def is-mac (= (.-platform js/process) "darwin"))
 
-(def template
-  [
-   (when is-mac
-     {:label   (.-name app)
-      :submenu [{:role "hide"}
-                {:role "hideOthers"}
-                {:role "unhide"}
-                {:type "separator"}
-                {:role "quit"}]})
-   {:label "Edit"
+(def mac-menu
+  [{:label   (.-name app)
+    :submenu [{:role "hide"}
+              {:role "hideOthers"}
+              {:role "unhide"}
+              {:type "separator"}
+              {:role "quit"}]}] )
+
+(def shared-menu
+  [{:label "Edit"
     :submenu [{:role "undo"}
               {:role "redo"}
               {:type "separator"}
@@ -27,12 +27,13 @@
               { :type "separator" },
               { :role "togglefullscreen" }]}
    {:label   "Window"
-    :submenu [{:role "minimize"}
-              (when is-mac
-                {:type "separator"}
-                {:role "front"})]}])
+    :submenu [{:role "minimize"}]}])
+
+(def final-menu
+    (concat (if is-mac mac-menu []) shared-menu))
 
 (defn init
   []
-  (let [menu (.buildFromTemplate Menu (clj->js template))]
+  (let [menu (.buildFromTemplate Menu (clj->js final-menu))]
     (.setApplicationMenu Menu menu)))
+
